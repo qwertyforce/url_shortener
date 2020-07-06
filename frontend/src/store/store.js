93bd31b114ae93db1b1141d885c9495a5e1148fa
qwertyforce <44163887+qwertyforce@users.noreply.github.com>
2020-07-06 19:38:02 +0300

    get rid of redux

--- frontend/src/store/store.js
+++
@@ -1,7 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import userDataReducer from '../redux_slices/userDataSlice';
-export default configureStore({
-  reducer: {
-    user_data: userDataReducer
-  },
-});
